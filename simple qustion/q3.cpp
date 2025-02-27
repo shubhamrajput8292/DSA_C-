e7781747dcd22fdc1adde12832fa6ed03228e059
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-02-28 00:34:39 +0530

    revison

---
+++ simple qustion/q3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter the number";
+    cin>>num;
+    if(num>=0)
+    {cout<<"postive number";
+    }
+    else
+    {
+        cout<<"negative number";
+    }
+    return 0;
+}
