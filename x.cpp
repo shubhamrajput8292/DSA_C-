ccd2bf27fcc1844e5486ae0ab82c455ee4962e11
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-03-15 16:20:22 +0530

    qustion.cpp

--- x.cpp
+++ x.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int i=5;
-    while(i<=1)
+    while(i>=1)
     {
         int j=1;
         while(j<=i)
@@ -13,7 +13,7 @@ int main()
 
         }
         cout<<endl;
-
+       i--;
     }
     return 0;
 }
