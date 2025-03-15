dd0783efd29a8e30d3ef95508ec87a3ca632472e
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-03-15 23:00:21 +0530

    sum of two number

--- youtub.cpp
+++ youtub.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 int main()
 {
-    float a=45;
-    float volume=(4/3)*3.14*a*a*a;
-    cout<<"Volume of sphere is "<<volume<<endl;
-    return 0;
+    int a=5;
+   
+    int b=6;
+    cout<<"sum of two numbers is";
+    cout<<a+b;
+    cout<<endl;
 
+    return 0;
 }
