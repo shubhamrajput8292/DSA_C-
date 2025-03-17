5f6f070b7dbce2a54cbf15cc118e6495ec348098
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-03-17 23:51:58 +0530

    absolute.cpp

---
+++ youtub..cpp/absolute.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<"enter the number";
+    if (n>=0)
+    {
+        cout<<n;
+    }
+    else
+    {
+        cout<<-n;
+
+    }
+ 
+}
