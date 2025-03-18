362f84b1327d2bfd55cdcff70e430cdd4a9b4de1
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-03-18 15:49:48 +0530

    add

---
+++ youtub..cpp/add three number.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int x;
+    
+    cout<<"enter the 1 number";
+    cin>>x;
+    int y;
+ 
+    cout<<"enter the 2 number";
+    cin>>y;
+    
+     int z;
+ 
+    cout<<"enter the 3 number";
+    cin>>z;
+    
+    
+    cout<< x+y+z;
+    return 0;
+}
