91f65243dabb0aee43c15ceea36903a4dc80d7da
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-03-18 23:46:19 +0530

    divisible by 5 and3

---
+++ youtub..cpp/divisible by 5and3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<"divisible the number"<<" ";
+    cin>>n;
+
+    if (n%3==0 &&n%5==0)
+{cout<<"divisible by 3 and 5";}
+
+else{
+    cout<<"not divisible by 3 and5";
+} 
+}
