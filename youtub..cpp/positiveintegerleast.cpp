56d0b1f9c8167b1ab19d1a9dc1233ce4305e7672
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-03-19 23:42:43 +0530

    leastnumber

---
+++ youtub..cpp/positiveintegerleast.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+    int a,b,c;
+    cout<<"enter the 1 number"<<" ";
+    cin>>a;
+    cout<<"enter the 2 number"<<" ";
+    cin>>b;
+cout<<"enter the 3 number"<<" ";
+cin>>c;
+
+if ( a<c&&a<b)
+{cout<<a<<"is the least";}
+else if (b<a&&b<c)
+{cout<<b<<"is the least";}
+else{
+    cout<<c<<"is the least";
+}                                                                          
+
+
+
+
+
+
+}
