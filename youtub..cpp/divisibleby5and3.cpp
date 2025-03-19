5836b0ddd1b0f41ddc4f26bc31db302b49863e23
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-03-19 22:53:31 +0530

    qustion

--- youtub..cpp/divisibleby5and3.cpp
+++ youtub..cpp/divisibleby5and3.cpp
@@ -6,10 +6,10 @@ int main ()
     cout<<"divisible the number"<<" ";
     cin>>n;
 
-    if (n%3==0 &&n%5==0)
-{cout<<"divisible by 3 and 5";}
+    if (n%3==0 ||n%5==0)
+{cout<<"divisible by 3 || 5";}
 
 else{
-    cout<<"not divisible by 3 and5";
+    cout<<"not divisible by 3 an||5";
 } 
 }
