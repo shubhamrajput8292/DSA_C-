c326c704dc76cd7ee0402a409dbf3c1270a948f4
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-03-22 11:59:32 +0530

    loopntable

---
+++ youtub..cpp/tablen.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the table";
+    cin>>n;
+    for(int i=n;i<=n*10;i=i+n)
+    {
+        if (i%n==0) cout<<i<<" ";
+    }
+}
