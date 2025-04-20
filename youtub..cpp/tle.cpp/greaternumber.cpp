bdb99e148e93afafd90ed4ab02f08fa1c17e3147
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-04-20 12:12:09 +0530

    elt

---
+++ youtub..cpp/tle.cpp/greaternumber.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+    int m;
+    cout<<"enter the number";
+    cin>>m;
+    if(n>=m)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<" no ";
+    }
+}
