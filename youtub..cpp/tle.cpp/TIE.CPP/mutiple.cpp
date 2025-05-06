b51ff9c26cd19bd3d51192a07f234b35b33cc7b7
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-05-07 00:29:48 +0530

    yes

---
+++ youtub..cpp/tle.cpp/TIE.CPP/mutiple.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the mutiple number";
+    cin>>n;
+    int m;
+    cout<<"enter the mutiple number";
+    cin>>m;
+    if(n%m==0)
+  
+    {
+        cout<<"multiple";
+    }
+    else
+    {
+        cout<<" not multiple ";
+    }
+}
